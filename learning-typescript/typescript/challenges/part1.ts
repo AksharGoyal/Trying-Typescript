fcec9ed38f5d72281dae15d47d146056bd403f3b
Akshar Goyal <claim.akshar@gmail.com>
2025-01-18 19:41:29 +0000

    added files

---
+++ learning-typescript/typescript/challenges/part1.ts
@@ -0,0 +1,9 @@
+function plus(x: number | string | undefined, y: number | string | undefined): number | string | undefined {
+    if (typeof x === "undefined" || typeof y === "undefined") return undefined;
+  if ((typeof x === "number") && (typeof y === "number")) return x + y;
+  if (typeof x === "string" && typeof y === "string") return x.concat(y);
+}
+
+console.log(plus(2025, 1));
+console.log(plus(undefined, 21));
+console.log(plus("StackUp", "Learn"));
---
+++ learning-typescript/typescript/enhanced-knowledge/unions.ts
@@ -0,0 +1,9 @@
+// type State = "on" | "off";
+
+// const status: State = "none";
+// console.log(status);
+
+type State = "on" | "off";
+
+const status: State = "on";
+console.log(status);
